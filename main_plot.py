04cd6714c3329777a8d524b4f1e1440be51532ce
roastduckie <68725644+roastduckie@users.noreply.github.com>
2020-08-05 02:20:50 -0500

    dataframe stable. need bokeh plotting
    Signed-off-by: roastduckie <kherring33&@gmail.com>

--- main_plot.py
+++ main_plot.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-url = 'https://github.com/roastduckie/Stationeers-Furnace-Plot/blob/master/recipes.csv'
-recipes = pd.read_csv(url, header=0, low_memory=False)
+url = 'https://raw.githubusercontent.com/roastduckie/Stationeers-Furnace-Plot/master/recipes.csv'
+recipes = pd.read_csv(url, index_col=0, header=0, low_memory=False)
 print(recipes)

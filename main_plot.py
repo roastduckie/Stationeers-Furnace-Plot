52e572b0406427328fc8e6c66e338d3ea62f00a4
roastduckie <kherring33&@gmail.com>
2020-08-09 23:03:20 -0500

    for loop creating coords, need to revisit tooltip formatting

--- main_plot.py
+++ main_plot.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from bokeh.layouts import row
+from bokeh.models import ColumnDataSource, HoverTool, Range1d
+from bokeh.palettes import Colorblind5
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import Range1d, ColumnDataSource
 
 #  Importing values from XML file in game data
 url = 'https://raw.githubusercontent.com/roastduckie/Stationeers-Furnace-Plot/master/recipes.csv'
-recipes = pd.read_csv(
+df = pd.read_csv(
     url,
     index_col=0,
     header=0,
@@ -13,28 +15,65 @@ recipes = pd.read_csv(
 
 output_file('index.html')
 #  making csv usable
-source = ColumnDataSource = ColumnDataSource(recipes)
-t_min = source.data['Min Temp'].tolist()
-t_max = source.data['Max Temp'].tolist()
-p_min = source.data['Min Pressure'].tolist()
-p_max = source.data['Max Pressure'].tolist()
+source = ColumnDataSource(df)
+
+hover = HoverTool()
+hover.tooltips = [
+    ('Minimum Pressure: ', '@MinPressure'),
+    ('Maximum Pressure: ', '@MaxPressure'),
+    ('Minimum Temperature: ', '@MinTemp'),
+    ('Maximum Temperature: ', '@MaxTemp'),
+    ('Materials: ', '@Materials')
+]
 
 #  creating the plot
-p = figure(
+standard = figure(
     plot_width=500,
     plot_height=500,
-    x_range=(0, 10000),
-    title='Furnace Recipes by Temperature vs Pressure',
+    x_range=(0, 2500),
+    title='Standard Alloy Temperatures and Pressures',
     x_axis_label='Temperature (K)',
     y_axis_label='Pressure (kPa)',
 )
 
-p.y_range = Range1d(0, 10000)
+standard.y_range = Range1d(0, 10000)
 
-p.patches(
-    xs=[t_min, t_min, t_max, t_max],
-    ys=[p_min, p_max, p_max, p_min],
-    alpha=0.5
+xs_standard = []
+ys_standard = []
+for i in range(7, 12, 1):
+   xs_standard.append([df.iloc[i, 2], df.iloc[i, 2], df.iloc[i, 3], df.iloc[i, 3]])
+   ys_standard.append([df.iloc[i, 0], df.iloc[i,1], df.iloc[i,1], df.iloc[i,0]])
+
+standard.patches(
+    xs=xs_standard,
+    ys=ys_standard,
+    alpha=0.2
 )
 
-show(p)
+standard.add_tools(hover)
+
+Super = figure(
+    plot_width=500,
+    plot_height=500,
+    x_range=(0, 2500),
+    title='Super Alloy Temperatures and Pressures',
+    x_axis_label='Temperature (K)',
+    y_axis_label='Pressure (kPa)',
+)
+
+Super.y_range = Range1d(0, 6000)
+
+xs_super = []
+ys_super = []
+for i in range(12, 17, 1):
+   xs_super.append([df.iloc[i, 2], df.iloc[i, 2], df.iloc[i, 3], df.iloc[i, 3]])
+   ys_super.append([df.iloc[i, 0], df.iloc[i,1], df.iloc[i,1], df.iloc[i,0]])
+
+Super.patches(
+    xs=xs_super,
+    ys=ys_super,
+    alpha=0.2
+)
+Super.add_tools(hover)
+
+# show(row(standard, Super))
